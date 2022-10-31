c3c5eb938e4590ceb0c01f2807f2e47b9eb85d39
s6553907 <101353546+s6553907@users.noreply.github.com>
2022-10-31 19:11:34 +0100

    URL Update
    https://streamingcommunity.golf/ Is no longer available.

--- StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
+++ StreamingcommunityProvider/src/main/kotlin/com/lagradost/StreamingcommunityProvider.kt
@@ -128,7 +128,7 @@ data class TrailerElement(
 
 class StreamingcommunityProvider: MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://streamingcommunity.golf"
+    override var mainUrl = "https://streamingcommunity.city"
     override var name = "StreamingCommunity"
     override val hasMainPage = true
     override val hasChromecastSupport = true
